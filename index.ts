42b448c25bb463e97436cadc19fe4cbdcecb2bf2
Oleg Klimenko <o.klimenko@2gis.ru>
2017-08-11 08:25:00 +0300

    Add translation json format specification

--- index.ts
+++ index.ts
@@ -22,3 +22,26 @@ export type PluralI18NEntry = {
 };
 
 export type I18NEntry = SingleI18NEntry | PluralI18NEntry;
+
+export interface TranslationMeta {
+  projectIdVersion: string,
+  reportMsgidBugsTo: string, // email
+  potCreationDate: string,
+  poRevisionDate: string,
+  lastTranslator: {
+    name: string,
+    email: string
+  },
+  language: string,
+  languageTeam: string,
+  pluralForms: string, // (n: number) => number
+  mimeVersion: string,
+  contentType: string,
+  contentTransferEncoding: string,
+  generatedBy: string
+}
+
+export interface TranslationJson {
+  meta?: TranslationMeta,
+  items: I18NEntry[]
+}
